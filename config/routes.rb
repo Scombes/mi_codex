674d902ce55352ddf004b94657fb6d92d00e8f37
Scombes <scott.combes@gmail.com>
2013-12-01 15:30:02 -0600

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 MiCodex::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root  'static_pages#home'
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
+  match '/signup', to: 'users#new',  via: 'get'
   match '/help',  to: 'static_pages#help',  via: 'get'
   match '/contact', to: 'static_pages#contact',  via: 'get'
-  match '/signup', to: 'users#new',  via: 'get'
+  
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
